dab0d3e0e235935683da5210a6d5949d50ab175a
MartinFredlund <martinfredlund@live.se>
2018-03-22 22:48:12 +0100

    test

---
+++ RoboCode/src/group07/Robot07.java
@@ -0,0 +1,62 @@
+package group07;
+import robocode.*;
+import java.awt.Color;
+
+public class Robot07 extends robocode.TeamRobot {
+	/**
+	 * run: Robot's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+		setColors(Color.red,Color.blue,Color.red); // body,gun,radar
+		//String[] teamm8 = getTeammates();
+		
+		
+		
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		 
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			turnGunRight(360);
+			ahead(100);
+			back(100);
+			
+			
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+	}
+	/**
+	 * onMessageReceived: What to do when you receive a message
+	 */
+	public void onMessageReceived(MessageEvent event) {
+		//Check if Message was from same type
+		boolean same = isTeammate("Robot07");
+		
+	}
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
+}
