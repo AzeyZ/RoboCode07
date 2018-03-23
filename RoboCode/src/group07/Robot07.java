ff455631a0ea2495b48d4b7039e847ee3b74a120
MartinFredlund <martinfredlund@live.se>
2018-03-23 10:56:09 +0100

    Testing Scan Robot

--- RoboCode/src/group07/Robot07.java
+++ RoboCode/src/group07/Robot07.java
@@ -25,9 +25,11 @@ public class Robot07 extends robocode.TeamRobot {
 		// Robot main loop
 		while(true) {
 			// Replace the next 4 lines with any behavior you would like
-			turnGunRight(360);
+			
+			turnRadarRight(360);
 			ahead(100);
 			back(100);
+			
 			
 //			try {
 //				sendMessage("Robot07","hejsan");
@@ -45,12 +47,19 @@ public class Robot07 extends robocode.TeamRobot {
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
+		
+		stop();
 		enemies.add(new Enemy(e.getEnergy()));
+		setTurnGunRight(getRadarHeading() - getGunHeading());
+		fire(1);
+		turnRight(e.getBearing()-90);
+		setTurnRadarRight(getHeading() - getRadarHeading() + e.getBearing());
+		
 	}
 	/**
 	 * onMessageReceived: What to do when you receive a message
 	 */
-	public void onMessageReceived(MessageEvent event) {
+	public void onMessageReceived(MessageEvent e) {
 		//Check if Message was from same type
 		
 		boolean m_Same = isTeammate("Robot07");
@@ -73,6 +82,9 @@ public class Robot07 extends robocode.TeamRobot {
 	 */
 	public void onHitWall(HitWallEvent e) {
 		// Replace the next line with any behavior you would like
+		
 		back(20);
 	}	
 }
+
+
