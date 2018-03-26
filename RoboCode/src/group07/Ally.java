059fc12590a8e0cb4108b660af3ba2a83eeca863
Andreas <aner96@gmail.com>
2018-03-26 21:49:44 +0200

    Ally 1.0

--- RoboCode/src/group07/Ally.java
+++ RoboCode/src/group07/Ally.java
@@ -3,14 +3,39 @@ package group07;
 
 public class Ally {
 	private String name;
+	private int X;
+	private int Y;
 	//etc
 	
 	public Ally(String name) {
 		this.name = name;
 	}
-	
+		
 	public boolean isSame() {
-		return this.name == "Robot07";
+		return this.name.contains("Robot07");
 	}
-	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getX() {
+		return X;
+	}
+
+	public void setX(int x) {
+		X = x;
+	}
+
+	public int getY() {
+		return Y;
+	}
+
+	public void setY(int y) {
+		Y = y;
+	}
 }
