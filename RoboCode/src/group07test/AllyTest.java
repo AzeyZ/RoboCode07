0ed0bac9d80087ca78284841cb64a05b137724b3
Ulfu <din_dr_elvis@live.se>
2018-03-28 15:38:32 +0200

    Added test 2 classes

---
+++ RoboCode/src/group07test/AllyTest.java
@@ -0,0 +1,27 @@
+package group07test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import group07.Ally;
+
+public class AllyTest {
+
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@After
+	public void tearDown() {
+		
+	}
+	
+	@Test
+	public void testAddEnemies() {
+		
+	}
+}
---
+++ RoboCode/src/group07test/TestAllClasses.java
@@ -0,0 +1,12 @@
+package group07test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({AllyTest.class})
+
+public class TestAllClasses {
+
+}
