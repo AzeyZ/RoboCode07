dfa5521efa25bf10d421b9a49c71c14182148724
Ulfu <din_dr_elvis@live.se>
2018-05-02 15:21:56 +0200

    98.9% messageWriter

--- RoboCode/src/group07test/MessageWriterTest.java
+++ RoboCode/src/group07test/MessageWriterTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import group07.Ally;
 import group07.EnemyBot;
 import group07.MessageWriter;
+import group07.Shots;
 
 public class MessageWriterTest {
 	double myXPos = 200;
@@ -48,21 +49,32 @@ public class MessageWriterTest {
 		String outputMsg; 
 		outputMsg = msg.standardMessage(myXPos, myYPos, allyList, enemyList, targetEnemy, tarXPos, tarYPos);
 		
-		assertEquals("Test if name is correct", "myPos;200.0;200.0\nfriendPos;Friend;0.0;0.0\nenemyDetails;;30.0;40.0;0.0;0.0;0.0;0\ntargetEnemy;SomeName\ntargetPos;100.0;100.0", outputMsg);
+		assertEquals("Test if standard message is correct", "myPos;200.0;200.0\nfriendPos;Friend;0.0;0.0\nenemyDetails;;30.0;40.0;0.0;0.0;0.0;0\ntargetEnemy;SomeName\ntargetPos;100.0;100.0", outputMsg);
 	}
 
 	@Test
 	public void shotTowardsAlly() {
-		// assertEquals("Test if y-cord is correct", y, ally.getY(), 0.01d);
+		ArrayList<Shots> shots = new ArrayList<Shots>();
+		Shots shot = new Shots(50, 39, 40);
+		shots.add(shot);
+		
+		String outputMsg; 
+		outputMsg = msg.shotTowardsAlly(myXPos, myYPos, name, shots);
+		assertEquals("Test if shotTowardsAlly message is correct", "myPos;200.0;200.0\ntargetEnemy;Robot07something\nrShot;50.0;39.0;40", outputMsg);
+
 	}
 
 	@Test
 	public void allyListUpdate() {
-		// assertEquals("Test if y-cord is correct", y, ally.getY(), 0.01d);
-	}
+		String outputMsg; 
+		outputMsg = msg.allyListUpdate(myXPos, myYPos, allyList);
+		assertEquals("Test if allyListUpdate message is correct", "myPos;200.0;200.0\nrAlly;Friend;0.0;0.0;0", outputMsg);
+}
 
 	@Test
 	public void enemyListUpdate() {
-		// assertEquals("Test if y-cord is correct", y, ally.getY(), 0.01d);
-	}
+		String outputMsg; 
+		outputMsg = msg.enemyListUpdate(myXPos, myYPos, enemyList);
+		assertEquals("Test if enemyListUpdate message is correct", "myPos;200.0;200.0\nrEnemy;0.0;0.0;0.0;0.0;0.0;0;", outputMsg);
+}
 }
