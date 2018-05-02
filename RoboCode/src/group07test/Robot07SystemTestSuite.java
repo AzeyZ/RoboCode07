26527ea13b12b7e3156a4089b4b84eb9a024e845
Ulfu <din_dr_elvis@live.se>
2018-05-02 14:20:09 +0200

    Test stuff messageWriter

--- RoboCode/src/group07test/Robot07SystemTestSuite.java
+++ RoboCode/src/group07test/Robot07SystemTestSuite.java
@@ -5,10 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({AllyTest.class, ShotsTest.class, TargetEnemyBotTest.class, 
-	MessageWriterTest.class})
-
-public class TestAllClasses {
+@SuiteClasses({ST_F1_WallsAvoided.class})
+public class Robot07SystemTestSuite {
 
 }
- 
