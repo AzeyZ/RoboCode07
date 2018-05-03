004b0a761a351bbbbecdfee92b798293f2d90441
Ulfu <din_dr_elvis@live.se>
2018-05-03 12:57:32 +0200

    RUT Q

--- RoboCode/src/group07test/Robot07SystemTestSuite.java
+++ RoboCode/src/group07test/Robot07SystemTestSuite.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ST_F1_WallsAvoided.class, 
-	ST_Q_1vs1SpinBot.class, ST_Q_1vs1Crazy.class})
+	ST_Q_1vs1SpinBot.class, ST_Q_1vs1Crazy.class, ST_Q_1vs1Rut.class})
 public class Robot07SystemTestSuite {
 
 }
