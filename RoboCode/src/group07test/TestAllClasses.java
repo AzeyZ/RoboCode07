7ddde616a1ba978c837cc2b5c9003b246e37e599
Ulfu <din_dr_elvis@live.se>
2018-05-08 16:56:42 +0200

    Update

--- RoboCode/src/group07test/TestAllClasses.java
+++ RoboCode/src/group07test/TestAllClasses.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ AllyTest.class, MessageWriterTest.class, 
-	EnemyBotTest.class, MathUtilsTest.class, 
+	EnemyBotTest.class, MathUtilsTest.class, SurfMovementTest.class, 
 	AllyTrackerTest.class, TargetEnemyPrioritizerTest.class, 
 	EnemyTrackerTest.class, MovementTest.class, EnemyTrackerTest.class, 
 	MessageHandlerTest.class })
